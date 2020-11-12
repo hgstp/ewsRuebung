3879a763334233c4cf1bd9d67ee7f4a47029c53d
Stephan Haug <haug@tum.de>
2020-11-12 11:32:17 +0100

    R skript update

--- mein_R_skript.R
+++ mein_R_skript.R
@@ -1,8 +1,9 @@
+n <- 50
 a <- 2
 b <- -3
 sig_sq <- 0.5
-x <- runif(40)
-y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+x <- runif(n)
+y <- a + b * x + rnorm(n, sd = sqrt(sig_sq))
 (x_n <- mean(x))
 #> [1] 0.579
 write(x_n, "mittelwert_x.txt")
